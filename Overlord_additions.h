338c1b2b4551c8c60723b7d42870189b51343af9
Andreas Schubert <schubsi@admin.mathema.de>
2016-03-31 17:51:31 +0200

    USB prinintg is working now!

---
+++ Overlord_additions.h
@@ -0,0 +1,31 @@
+/* This file contains addtition neccesary to compile for Overlord */
+
+
+// Code seen at https://github.com/FastLED/FastLED
+namespace __cxxabiv1
+{
+
+        /* guard variables */
+
+        /* The ABI requires a 64-bit type.  */
+        __extension__ typedef int __guard __attribute__((mode(__DI__)));
+
+        extern "C" int __cxa_guard_acquire (__guard *) __attribute__((weak));
+        extern "C" void __cxa_guard_release (__guard *) __attribute__((weak));
+        extern "C" void __cxa_guard_abort (__guard *) __attribute__((weak));
+
+        extern "C" int __cxa_guard_acquire (__guard *g)
+        {
+                return !*(char *)(g);
+        }
+
+        extern "C" void __cxa_guard_release (__guard *g)
+        {
+                *(char *)g = 1;
+        }
+
+        extern "C" void __cxa_guard_abort (__guard *)
+        {
+
+        }
+}
